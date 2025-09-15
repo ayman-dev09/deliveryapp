a6f3e29326968cb2c2d015739f47334feebaedf6
ayman Dev <ayman.dev09@gmail.com>
2025-09-15 23:29:38 +0300

    15-09-2025 home

--- lib/view/pages/home_screen_widgets/horizontal_list_widget.dart
+++ lib/view/pages/home_screen_widgets/horizontal_list_widget.dart
@@ -9,19 +9,25 @@ class HorizontalListWidget extends StatelessWidget {
       padding: const EdgeInsets.only(top: 20),
       child: Container(
         width: double.infinity,
-        height: 350,
+        height: 260,
         color: Colors.blue,
         child: Padding(
-          padding: const EdgeInsets.only(top: 15, left: 25),
+          padding: const EdgeInsets.only(top: 20, left: 25),
           child: SizedBox(
-            width: 120,
             height: 220,
             child: ListView.builder(
               itemCount: 10,
               shrinkWrap: true,
               scrollDirection: Axis.horizontal,
               itemBuilder: (_, index) {
-                return item();
+                return Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: item(),
+                    ),
+                  ],
+                );
               },
             ),
           ),
@@ -31,6 +37,13 @@ class HorizontalListWidget extends StatelessWidget {
   }
 
   Widget item() {
-    return Container(width: 100, height: 200, color: Colors.amber);
+    return Container(
+      width: 160,
+      height: 220,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.amber,
+      ),
+    );
   }
 }
