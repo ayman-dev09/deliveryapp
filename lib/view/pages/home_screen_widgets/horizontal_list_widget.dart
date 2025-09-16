f89a7aff380aaacfbf8bb2eb63edfb3b675d524b
ayman Dev <ayman.dev09@gmail.com>
2025-09-16 23:32:51 +0300

    16-09-2025 home

--- lib/view/pages/home_screen_widgets/horizontal_list_widget.dart
+++ lib/view/pages/home_screen_widgets/horizontal_list_widget.dart
@@ -44,6 +44,35 @@ class HorizontalListWidget extends StatelessWidget {
         borderRadius: BorderRadius.circular(10),
         color: Colors.amber,
       ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
+            child: Image.asset(
+              'assets/list_food_images/burger.png',
+              width: double.maxFinite,
+              height: 100,
+              fit: BoxFit.contain,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10, left: 10, right: 20),
+            child: Text('King Chicken Crispy combo'),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5, left: 10, right: 20),
+            child: Text(
+              '20 SAR',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10, left: 10, right: 20),
+            child: Text('30-40 min  6.2 km'),
+          ),
+        ],
+      ),
     );
   }
 }
